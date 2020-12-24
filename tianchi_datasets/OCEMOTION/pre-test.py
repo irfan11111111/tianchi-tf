192a6afa776524668c15c91a5437e43a7c357850
irfan11111111 <45596321+irfan11111111@users.noreply.github.com>
2020-12-24 08:59:05 +0800

    Add files via upload

---
+++ tianchi_datasets/OCEMOTION/pre-test.py
@@ -0,0 +1,20 @@
+
+# import pandas as pd
+# import numpy as np
+# data=pd.read_csv("./OCEMOTION_a.csv")
+# print(data.columns)
+
+import csv
+sample_list=[]
+c=open("./OCEMOTION_a.csv","r",encoding="utf-8")
+read=csv.reader(c)
+for line in read:
+    sample=(str(line[0])+"	happiness").split("	")
+    print(sample)
+    sample_list.append(sample)
+c.close()
+c=open("./test.csv","w",encoding="utf-8",newline='')
+for sample in sample_list:
+    writer=csv.writer(c)
+    writer.writerow(sample)
+c.close()
